873cd97a6df31b97b24dea0633e2d1fb599d7694
koufounak <koufounakis1@hotmail.com>
2018-12-19 02:38:04 +0200

    Added 2nd GET method (tweets?morethan=x)

--- return_all_tweets.py
+++ return_all_tweets.py
@@ -1,7 +1,8 @@
 import pymongo
+from flask import Flask, request
 
 client = pymongo.MongoClient('localhost', 27017)
-db = client['admin']
+db = client['tweets']
 collection = db['pspi students']
 
 
@@ -26,11 +27,36 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class all_tweets(Resource):
     def get(self):
-        return all_documents
+        x = request.args.get('morethan', default=-1, type=int)
+        #if no parameters (i.e x=-1 by default) then return all tweets(documents)
+        if(x == -1):
+            return all_documents
 
-api.add_resource(HelloWorld, '/tweets')
+        #if x != -1 return all tweets where num_of_hashtags > x
+        else:
+            # List of all tweets with more than x hashtags
+            more_than_x_tweets = list()
+            cursor = collection.find({})
+            #for each json object in the collection convert Mongo objects to string
+            # and access 'hashtags' list to get its length
+            for document in cursor:
+
+                document['_id'] = str(document['_id'])
+                document['created_at'] = str(document['created_at'])
+                num_of_hashtags = len(document['entities']['hashtags'])
+
+                #Append all documents that hashtags > x to the list.
+                if(num_of_hashtags > x):
+                    more_than_x_tweets.append(document)
+
+            return more_than_x_tweets
+
+
+
+api.add_resource(all_tweets, '/tweets')
+#api.add_resource(more_than_tweets, '/tweets')
 
 if __name__ == '__main__':
     app.run(debug=True)
